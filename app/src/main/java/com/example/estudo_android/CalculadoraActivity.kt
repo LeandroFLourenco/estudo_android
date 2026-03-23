83d89c7b2ce46998d00d8b9c0f44430b65792eea
LeandroFLourenco <leandrinlourenco@gmail.com>
2026-03-22 22:06:49 -0300

    feat: Foi realizado estudo de evento onclick e criado nova pagina com design aproximado de uma calculadora

--- app/src/main/java/com/example/estudo_android/CalculadoraActivity.kt
+++ app/src/main/java/com/example/estudo_android/CalculadoraActivity.kt
@@ -1,28 +1,16 @@
 package com.example.estudo_android
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CalculadoraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val botaoAbrir = findViewById<Button>(R.id.btnAbrir)
-
-        botaoAbrir.setOnClickListener {
-
-            val intent = Intent(this, CalculadoraActivity::class.java)
-            startActivity(intent)
-
-        }
-
+        setContentView(R.layout.activity_calculadora)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
